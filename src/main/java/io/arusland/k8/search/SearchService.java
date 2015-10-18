fef34c579aaba26b0ea0870eb16d0101b5354fa3
Ruslan Absalyamov <ruslan.absalyamov@gmail.com>
2015-10-18 17:58:34 +0300

    Added comments to SearchService.

--- src/main/java/io/arusland/k8/search/SearchService.java
+++ src/main/java/io/arusland/k8/search/SearchService.java
@@ -8,11 +8,23 @@ import java.util.List;
  * Created by ruslan on 28.06.2015.
  */
 public interface SearchService {
+    /**
+     * Search objects by filter
+     */
     List<SearchObject> search(SearchFilter filter);
 
+    /**
+     * Search objects in public index with default settings.
+     */
     List<SearchObject> search(String searchText);
 
+    /**
+     * Index specified object.
+     */
     void index(SearchObject object);
 
+    /**
+     * Explicitly flush all indices.
+     */
     void flush();
 }
